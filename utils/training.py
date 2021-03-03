ce7298a5297dd9188680d02016cb31f57efcc520
scottjeen <scottjeen@gmail.com>
2021-03-03 15:38:09 +0000

    various changes

---
+++ utils/training.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+
+def TRAIN(env, model, learning_rate, loss='mse'):
+    """
+    Train a model given an environment and a model.
+
+    Parameters
+    ----------
+        env:            Environment
+        model:          Model class
+        learning_rate:  Learning rate
+        loss:           Loss type
+
+    """
+    return Pass
